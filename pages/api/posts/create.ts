75e74c45ea403bd2753913af5b6d805961dae02a
srikar-samudrala <srikar@eve.world>
2022-07-07 16:21:32 +0530

    implements create post

---
+++ pages/api/posts/create.ts
@@ -0,0 +1,53 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Post } from '@prisma/client';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import slugify from 'slugify';
+import { z } from 'zod';
+
+import { db } from '../../../prisma';
+import { authCheck } from '../../../utils/auth-check';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Post | { error: string } | { message: string }>
+) {
+  if (req.method !== 'POST') {
+    res.status(405).send({ message: 'Only POST requests allowed' });
+    return;
+  }
+
+  try {
+    const user = await authCheck(req, res);
+    if (!user) {
+      return;
+    }
+
+    const validated = PostPayloadValidator.parse(req.body);
+    const slug = slugify(validated.title, {
+      lower: true,
+    });
+
+    const post = await db.post.create({
+      data: {
+        ...validated,
+        slug,
+        author: {
+          connect: {
+            username: user.username,
+          },
+        },
+      },
+    });
+
+    res.status(200).json(post);
+    return;
+  } catch (err) {
+    res.status(500).json({ error: (err as Error).message });
+  }
+}
+
+const PostPayloadValidator = z.object({
+  title: z.string().min(1, { message: 'Title is missing' }),
+  body: z.string().optional(),
+  subName: z.string().min(1, { message: 'subname is missing' }),
+});
